b5e46dc0160c251d0bbce62875371a23aaaaab3c
visal <phoung.visal@institute.pse.ngo>
2025-10-14 08:55:44 +0700

    first commit

---
+++ src/routes/bookRoute.ts
@@ -0,0 +1,8 @@
+import express from "express";
+import { createBook } from "@/controllers/bookController";
+
+const router = express.Router();
+
+router.post("/create-book", createBook);
+
+export default router;
---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import connectDB from "./config/database";
+import router from "./routes/index";
+const app = express();
+//Enable json parsing body
+app.use(express.json());
+
+//Enable URL-encoded parsing body parsing with extended mode
+//`extended : true` allow rich objects and arrays vai query string library
+app.use(express.urlencoded({ extended: true }));
+
+
+connectDB();
+
+app.use("/api/v1", router);
+
+app.listen(4000, () => {
+  console.log(`server run on port 4000`);
+});
