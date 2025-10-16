fa2399deb45461e8b09c839c18d1587f8d2a6d8e
Your Name <thoeurn.bunthen@institute.pse.ngo>
2025-10-16 15:26:03 +0700

    deletebook

--- src/controllers/bookController.ts
+++ src/controllers/bookController.ts
@@ -2,6 +2,7 @@ import { createBookService } from "@/services/bookService";
 import { CreateBookInput } from "@/types/book";
 import { Request, Response } from "express";
 import { getAllBooksService } from "@/services/bookService"; // after creating the getAllBooksService, we need to create the controller for it
+import { deleteBookByIdService } from "@/services/bookService";
 
 export const createBook = async (req: Request, res: Response) => {
   try {
@@ -23,7 +24,19 @@ export const getAllBooks = async (req: Request, res: Response) => {
     res.status(200).json(result);
   } catch (error) {
     res.status(500).json({ success: false, message: "seever error" });
-    
-
   }
 }; // After creating the getAllBooks, we need to create the route for it
+
+export const deleteBookById = async (req: Request, res: Response) => {
+  try {
+    const bookId = req.params.id;
+    const result = await deleteBookByIdService(bookId);
+    if (result.success) {
+      res.status(200).json(result);
+    } else {
+      res.status(404).json(result);
+    }
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Server error" });
+  }
+};
