6abd8a6ab1d8b67618d2ed77861dbaf371624fc0
Your Name <thoeurn.bunthen@institute.pse.ngo>
2025-10-23 09:05:16 +0700

    create getAllcategory

--- src/routes/categoryRoutes.ts
+++ src/routes/categoryRoutes.ts
@@ -1,12 +1,31 @@
 import express from "express";
-import {UserRole} from "../enum/user-enum";
-import { createCategoryController, getCategoryByIdController, updateCategoryController, deleteCategoryController } from "../controllers/categoryController";
+import { UserRole } from "../enum/user-enum";
+import {
+  createCategoryController,
+  getCategoryByIdController,
+  updateCategoryController,
+  deleteCategoryController,
+  getAllcategoriesController,
+} from "../controllers/categoryController";
 export const rouer = express.Router();
 import { roleCheck } from "../middlewares/roleMiddleware";
+import { get } from "http";
 const router = express.Router();
-router.post("/create-category", roleCheck([UserRole.ADMIN]), createCategoryController); 
-router.put("/update-category/:id", roleCheck([UserRole.ADMIN]), updateCategoryController);
-router.delete("/delete-category/:id", roleCheck([UserRole.ADMIN]), deleteCategoryController);
+router.post(
+  "/create-category",
+  roleCheck([UserRole.ADMIN]),
+  createCategoryController
+);
+router.put(
+  "/update-category/:id",
+  roleCheck([UserRole.ADMIN]),
+  updateCategoryController
+);
+router.delete(
+  "/delete-category/:id",
+  roleCheck([UserRole.ADMIN]),
+  deleteCategoryController
+);
 router.get("/:id", getCategoryByIdController);
-
+router.get("/", getAllcategoriesController);
 export default router;
