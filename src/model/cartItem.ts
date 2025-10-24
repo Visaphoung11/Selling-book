192bf929eb2b42184bbb3d3ce68aa03eee984cde
kimhong <try.kimhong@institute.pse.ngo>
2025-10-24 15:49:40 +0700

    update code

--- src/model/cartItem.ts
+++ src/model/cartItem.ts
@@ -1,1 +1,17 @@
+import { Cart } from "./cart";
+import { Book } from "./book";
 
+@Entity()
+export class CartItem {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @ManyToOne(() => Cart, (cart) => cart.items)
+  cart: Cart;
+
+  @ManyToOne(() => Book)
+  book: Book;
+
+  @Column()
+  quantity: number;
+}
